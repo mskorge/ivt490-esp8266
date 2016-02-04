4c74f213ccf0f7f600424e73393e2b359c1d57ba
Michael Skorge <michael.skorge@swedol.se>
2016-02-04 21:16:02 +0100

    Display layout and comments

--- Sonos/sonos.lua
+++ Sonos/sonos.lua
@@ -1,22 +1,25 @@
+-- Display layout
 function drawOLED()
     disp:firstPage()
         repeat
-            disp:drawStr(0, 0, track)
-            disp:drawStr(0, 10, artist)
+            disp:drawLine(0, 18, 128, 18)
+            disp:drawStr(0, 0, "2000W")
+            disp:drawStr(100, 0, "10 C")
+            disp:drawStr(0, 20, track)
+            disp:drawStr(0, 40, artist)
     until disp:nextPage() == false
 end
 
--- on publish message receive event
+-- On incoming message, update display
 m:on("message", function(conn, topic, data) 
-  --print(topic .. ":" ) 
-  --if data ~= nil then
-    --print(data)
-  --end
   if topic == "/sonos/livingroom/track" then
     track = data
   end
   if topic == "/sonos/livingroom/artist" then
     artist = data
+  end
+  if topic == "/ivy/out" then
+    out = data
   end
   if artist ~= nil and track  ~= nil then
   print(artist.." - "..track)
@@ -24,6 +27,7 @@ m:on("message", function(conn, topic, data)
   end
 end)
 
+-- Connect to server and subscribe
 m:connect(MQTT_HOST, MQTT_PORT, 0, function(conn) 
     print("connected") 
     m:publish("/esp/"..NODE_ID,"hello world from "..NODE_ID,0,0, function(conn) 
@@ -31,6 +35,9 @@ m:connect(MQTT_HOST, MQTT_PORT, 0, function(conn)
     end)
     m:subscribe("/sonos/livingroom/#",0, function(conn) 
         print("Subscribed to livingroom") 
+    end)
+    m:subscribe("/ivt/#",0, function(conn) 
+        print("Subscribed to ivt") 
     end)
 end)
 
